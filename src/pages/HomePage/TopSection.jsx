bfaa1f1006ae73d6e8806cb37c2b33dda4cf6a5c
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-10-20 10:54:36 +0600

    added json file

--- src/pages/HomePage/TopSection.jsx
+++ src/pages/HomePage/TopSection.jsx
@@ -24,7 +24,7 @@ const TopSection = () => {
       {/* Text on top of the Lottie animation */}
       <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center text-center">
         <div className="text-center md:w-1/2">
-          <h1 className="text-6xl font-extrabold text-dharohs">Dherosh</h1>
+          <h1 className="text-6xl font-extrabold text-dharosh">Dherosh</h1>
           <p className="text-white text-xl pt-2">
             Your Gateway to Innovative Software Solutions. We specialize in crafting tailored, high-quality software products for businesses and individuals, driving digital transformation with creativity and
             expertise.
